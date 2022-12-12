768286605324227e8624606f165331df66957be1
KristianKjerstad <kristian.kjerstad@bouvet.no>
2022-12-12 15:12:02 +0100

    chore: generate dmss api

--- dm_cli/dmss_api/api/default_api.py
+++ dm_cli/dmss_api/api/default_api.py
@@ -3299,7 +3299,7 @@ class DefaultApi(object):
 
             Keyword Args:
                 scope (dict): [optional]
-                time_to_live (int): [optional]
+                time_to_live (int): [optional] if omitted the server will use the default value of 2592000
                 _return_http_data_only (bool): response data without head status
                     code and headers. Default is True.
                 _preload_content (bool): if False, the urllib3.HTTPResponse object
