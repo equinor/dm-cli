4f1313ed6af57929f1d9e817c8d76c4356c101af
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-08-24 11:02:12 +0200

    ci: update release please to use release-type python

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
 with open("requirements.txt", "r") as fh:
     requirements = fh.read()
 
-from dm_cli import VERSION
+
 
 setup(
     name="dm-cli",
-    version=VERSION,
+    version="1.1.0",
     author="",
     author_email="",
     license="MIT",
